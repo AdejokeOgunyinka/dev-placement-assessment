f574f85cb33cf8add52d44b049fee9323db9cf56
“AdejokeOgunyinka” <ogunyinkajoke@yahoo.com>
2021-01-14 10:18:19 +0100

    Testing Continuous Integration

--- src/Components/SpecificUser/index.js
+++ src/Components/SpecificUser/index.js
@@ -5,7 +5,7 @@ import Input from '../Input/index';
 import LinkBox from '../Link/index';
 import Thumbnail from '../Thumbnail/index';
 // import Card from '../Card/index';
-import {useHistory } from 'react-router-dom';
+// import {useHistory } from 'react-router-dom';
 import {
 	HeaderStyle,
 	WelcomeStyle,
